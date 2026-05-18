6486f671b42a1315db4a2c1e59e0bbbcaf035a1f
sleepywoods <danieltanyl91@gmail.com>
2026-05-19 00:35:19 +0800

    Validate canonical vendor category tags

--- tests/test_region_taxonomy.py
+++ tests/test_region_taxonomy.py
@@ -1,4 +1,9 @@
-from tools.openva.validate import load_region_tags, validate_region_tags
+from tools.openva.validate import (
+    load_region_tags,
+    load_vendor_category_tags,
+    validate_region_tags,
+    validate_vendor_category_tags,
+)
 
 
 def test_region_taxonomy_includes_core_market_tags():
@@ -25,4 +30,26 @@ def test_region_tags_reject_unknown_values():
 
     assert failures == [
         "data/vendors/example/vendor.yaml: regions_served tag asia-pacific is not defined in config/region-taxonomy.yaml"
+    ]
+
+
+def test_vendor_category_tags_accept_controlled_values():
+    failures = validate_vendor_category_tags(
+        "data/vendors/example/vendor.yaml",
+        ["cloud_infrastructure"],
+        load_vendor_category_tags(),
+    )
+
+    assert failures == []
+
+
+def test_vendor_category_tags_reject_unknown_values():
+    failures = validate_vendor_category_tags(
+        "data/vendors/example/vendor.yaml",
+        ["finance_platform"],
+        load_vendor_category_tags(),
+    )
+
+    assert failures == [
+        "data/vendors/example/vendor.yaml: vendor_categories tag finance_platform is not defined in config/category-taxonomy.yaml"
     ]
